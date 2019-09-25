a3a5ffb74d12f909d7117f5957eb712e1b6c5bae
LUIS J Alvarez Diaz <ldiaz244@gmail.com>
2019-09-25 21:40:16 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,9 @@
+Class Artist
+attr_accessor = artist.all
+@@artist = artist.all
+artist.add_song
+artist.find_or_create_by_name
+artist.print_songs
+Song.all
+attr_accessor = song.all
+song.new_by_filename 
